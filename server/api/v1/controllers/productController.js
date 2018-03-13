1aa402682d631d7e117603e5bb0b8442bbd94a2f
Teslenko Roman <r.teslenko@i-sys.ru>
2018-03-13 20:23:22 +0500

    [I-3] - Добавление API, на серверной стороне, для взаимодействия с продуктами

--- server/api/v1/controllers/productController.js
+++ server/api/v1/controllers/productController.js
@@ -10,6 +10,17 @@ exports.getProducts = function(req, res) {
   })
 }
 
+exports.getProduct = function(req, res) {
+  var productId = req.params.id
+
+  // mongoose.connect(url)
+  Product.findById(productId).populate('category').exec(function(err, product) {
+    // mongoose.disconnect()
+    if (err) return res.status(400).send()
+    res.json(product)
+  })
+}
+
 exports.createProduct = function(req, res) {
   if (!req.body) return res.status(404).send()
 
@@ -32,5 +43,40 @@ exports.createProduct = function(req, res) {
       // mongoose.disconnect()
       res.status(400).send()
     }
+  })
+}
+
+exports.deleteProduct = function(req, res) {
+  var id = req.params.id
+
+  console.log(req.params)
+
+  // mongoose.connect(url)
+  Product.findByIdAndRemove(id, function(err, doc) {
+    // mongoose.disconnect()
+    if (err) return res.status(400).send()
+    res.json(doc)
+  })
+}
+
+exports.updateProduct = function(req, res) {
+  if (!req.params) return res.status(404).send()
+
+  var productId = req.params.id
+  var productData = req.body.data
+
+  // mongoose.connect(url)
+
+  Product.findById(productId, function(err, product) {
+    product.name = productData.name
+    product.save()
+      .then(function(product) {
+        // mongoose.disconnect()
+        res.json(product)
+      })
+      .catch(function(err) {
+        // mongoose.disconnect()
+        res.status(400).send()
+      })
   })
 }
--- server/api/v1/routes/productRoutes.js
+++ server/api/v1/routes/productRoutes.js
@@ -4,4 +4,9 @@ module.exports = function(app) {
     app.route('/api/v1/products')
         .get(product.getProducts)
         .post(product.createProduct)
+        
+    app.route('/api/v1/products/:id')
+        .get(product.getProduct)
+        .delete(product.deleteProduct)
+        .put(product.updateProduct)
 }
