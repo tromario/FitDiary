846d2af4b44cbece3fe253370a89290b68905549
Teslenko Roman <r.teslenko@i-sys.ru>
2018-03-14 21:40:22 +0500

    [I-3] - Удаление устаревшего кода

--- app/reducers/product.js
+++ app/reducers/product.js
@@ -6,31 +6,6 @@ const initialState = {
 }
 
 export default function product(state = initialState, action) {
-  // switch (action.type) {
-  //   case GET_PRODUCTS_REQUEST:
-  //     return { ...state }
-
-  //   case GET_PRODUCTS_SUCCESS:
-  //     return { ...state, products: action.payload }
-
-  //   case ADD_PRODUCT_REQUEST:
-  //     return { ...state }
-
-  //   case ADD_PRODUCT_SUCCESS:
-  //     return {
-  //       products: [
-  //         ...state.products,
-  //         action.payload
-  //       ]
-  //     }
-
-  //   case DELETE_PRODUCT:
-  //     return { ...state, products: state.products.filter(product => product.id !== action.payload) }
-
-  //   default:
-  //     return state;
-  // }
-
   switch (action.type) {
     case types.CREATE_PRODUCT_SUCCESS:
       return {
