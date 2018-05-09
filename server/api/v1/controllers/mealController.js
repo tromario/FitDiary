c6a3050c368899e48919a1ca120530192087c946
Teslenko Roman <r.teslenko@i-sys.ru>
2018-05-09 09:02:03 +0500

    [I-7] - Доработка контроллера приема пищи для работы с историей

--- server/api/v1/controllers/mealController.js
+++ server/api/v1/controllers/mealController.js
@@ -76,29 +76,9 @@ exports.createMeal = function(req, res) {
     var mealData = req.body.data;
     var meal = new Meal(mealData);
 
-    console.log('data from query - createMeal: ', mealData);
+    console.log('data from query - createMeal: ', mealData);        
 
-    var date = meal.date;
-    var today = moment(date).startOf('day');
-    var tomorrow = moment(today).add(1, 'days');
-
-    var getHistory = new Promise(function(resolve, reject) {
-        History
-            .findOne({ date: {$gte: today.toDate(), $lt: tomorrow.toDate()} })
-            .exec(function(err, docs) {
-                if (!err && docs) {
-                    resolve(docs);
-                } else {
-                    var history = new History({ date: date })
-                        .save(function(error) {
-                            if (error) return reject(error);
-                            resolve(history);
-                        });
-                }
-            })
-    })
-
-    getHistory
+    updateCalculatingHistory(meal)
         .then(function(history) {
             meal.history = history.id;
             meal.save(function(error) {
@@ -118,6 +98,60 @@ exports.createMeal = function(req, res) {
         }).catch(function(error) {
             console.log('error from getHistory: ', error);
         });
+}
+
+var getHistory = function(date) {
+    return new Promise(function(resolve, reject) {
+        var today = moment(date).startOf('day');
+        var tomorrow = moment(today).add(1, 'days');
+        
+        History
+            .findOne({ date: {$gte: today.toDate(), $lt: tomorrow.toDate()} })
+            .populate('meals')
+            .exec(function(err, docs) {
+                if (!err && docs) {
+                    resolve(docs);
+                } else {
+                    var history = new History({ date: date })                    
+                        .save(function(error) {
+                            if (error) return reject(error);
+                            resolve(history);
+                        });
+                }
+            })
+    });
+}
+
+var updateCalculatingHistory = function(meal) {
+    return new Promise(function(resolve, reject) {
+        var date = meal.date;
+
+        getHistory(date)
+            .then(function(history) {
+                history.meals.push(meal);
+
+                history.totalAmount = 0;
+                history.totalProteins = 0;
+                history.totalFats = 0;
+                history.totalCarbohydrates = 0;
+                history.totalCellulose = 0;
+                history.totalCaloricity = 0;
+                history.totalEnergy = 0;
+                history.meals.map(function(meal) {
+                    history.totalAmount += meal.totalAmount;
+                    history.totalProteins += meal.totalProteins;
+                    history.totalFats += meal.totalFats;
+                    history.totalCarbohydrates += meal.totalCarbohydrates;
+                    history.totalCellulose += meal.totalCellulose;
+                    history.totalCaloricity += meal.totalCaloricity;
+                    history.totalEnergy += meal.totalEnergy;
+                });
+                history.save(function(error) {
+                    if (error) return reject(error);
+                    resolve(history);
+                });
+            });
+    });
 }
 
 exports.deleteMeal = function(req, res) {
