e18528737c5444808d01ebfe04df5ee8408ad78c
Teslenko Roman <r.teslenko@i-sys.ru>
2018-05-09 09:01:10 +0500

    [I-7] - Модель для формировании истории по приемам пищи

--- server/api/v1/models/historyModel.js
+++ server/api/v1/models/historyModel.js
@@ -6,6 +6,8 @@ const historySchema = new Schema(
         //_id: { type: Schema.Types.ObjectId, require: false },
         // дата приема
         date: { type: Date, default: Date.now },
+        // приемы пищи
+        meals: [{ type: Schema.Types.ObjectId, ref: 'Meal', required: false }],
         // количество
         totalAmount: { type: Number, default: 0, required: false },
         // белки
@@ -22,8 +24,24 @@ const historySchema = new Schema(
         totalEnergy: { type: Number, default: 0, required: false }
     },
     {
-        versionKey: false
+        versionKey: false,
+        
+        // MongoError: Unknown modifier: $pushAll
+        // https://medium.com/@stefanledin/how-to-solve-the-unknown-modifier-pushall-error-in-mongoose-d631489f85c0
+        usePushEach: true 
     }
 )
+
+// TODO: Выяснить, что эффективнее: виртуальный метод или поле в таблице, связанное по id 
+/*
+historySchema.virtual('meals', {
+    ref: 'Meal',
+    localField: '_id',
+    foreignField: 'history'
+})
+
+historySchema.set('toObject', { virtuals: true });
+historySchema.set('toJSON', { virtuals: true });
+*/
 
 module.exports = mongoose.model('History', historySchema)
