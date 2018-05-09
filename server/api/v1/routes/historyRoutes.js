285f9ef5c3aa658dd0715008f5202c25ad5d8acb
Teslenko Roman <r.teslenko@i-sys.ru>
2018-05-09 09:54:47 +0500

    [I-7] - Выделение запроса на получение всех и отфильтрованных приемов пищи в контроллер истории

---
+++ server/api/v1/routes/historyRoutes.js
@@ -0,0 +1,6 @@
+module.exports = function(app) {
+    const historyController = require('../controllers/historyController')
+
+    app.route('/api/v1/histories')
+        .get(historyController.getHistories)
+}
